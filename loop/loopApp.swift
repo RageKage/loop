da8d958a1980e44872dbaca2b9bb031feaf86d76
Niman Ahmed <Nimanahmed000@gmail.com>
2026-04-17 22:05:01 -0500

    Initial Commit

---
+++ loop/loopApp.swift
@@ -0,0 +1,32 @@
+//
+//  loopApp.swift
+//  loop
+//
+//  Created by Niman Ahmed on 4/17/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct loopApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
